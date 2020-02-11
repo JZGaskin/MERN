4a4d15572d47c05d9e1d15cd0c3386b2fccd70df
Your NameZachGaskin <zgaskin@ufclab.com>
2020-02-11 09:15:37 -0500

    version 1.0.0

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://JZGaskin:Accuplex41!@mernshoppinglist-1vsor.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const items = require("./routes/api/items");
+
+const app = express();
+
+// Middleware was bodyParser(now in express)
+app.use(express.json());
+
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch(err => console.log(err));
+
+//Use Routes
+app.use("/api/items", items);
+
+//Serve static assets IF in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+//create variable for the port we want to use
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
